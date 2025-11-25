e02928aba4149e8df5f943b934a0da532cedee99
samirstha2002 <154325590+samirstha2002@users.noreply.github.com>
2025-11-25 16:18:10 +0545

    date username and caht implementaion

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,32 @@
 const chatForm = document.getElementById("chat-form");
-
+const chatMessages = document.querySelector(".chat-messages");
 const socket = io();
 
+//message from server
 socket.on("message", (message) => {
   console.log(message);
+  outputMessage(message);
+
+  // scroll down
+  chatMessages.scrollTop = chatMessages.scrollHeight;
 });
 
 chatForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const msg = e.target.elements.msg.value;
-  console.log(msg);
+  //emit message to server
+  socket.emit("chatMessage", msg);
+
+  // clear message in put
+  e.target.elements.msg.value = "";
+  e.target.elements.msg.focus();
 });
+
+//  OUtput msg to the Dom
+
+function outputMessage(message) {
+  const div = document.createElement("div");
+  div.classList.add("message");
+  div.innerHTML = `<p class="meta">Brad <span>9:12pm</span></p> <p class="text">	${message}</p>`;
+  document.querySelector(".chat-messages").appendChild(div);
+}
--- server.js
+++ server.js
@@ -7,24 +7,32 @@ const socketio = require("socket.io");
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
-
+const formatMessages = require("./utils/messages");
 // middle ware for static files
 app.use(express.static(path.join(__dirname, "public")));
 
+const botName = "Chat-Cord Bot";
 // run when clients connect
 io.on("connection", (socket) => {
   //   console.log("NEW WS connection.... ");
 
   // Welcome current user
-  socket.emit("message", "Welcome to ChatCord!");
+  socket.emit("message", formatMessages(botName, "Welcome to Chat-Cord!"));
 
   // broadcast the all user except current user in room
-  socket.broadcast.emit("message", "user has joined the chat");
+  socket.broadcast.emit(
+    "message",
+    formatMessages(botName, "user has joined the chat")
+  );
 
   // disconnect the current user
   socket.on("disconnect", () => {
     // broadcast to all user including current one
-    io.emit("message", "user has left the chat");
+    io.emit("message", formatMessages(botName, "user has left the chat"));
+  });
+  // listen message
+  socket.on("chatMessage", (msg) => {
+    io.emit("message", formatMessages("USER", msg));
   });
 });
 const port = process.env.PORT || 3000;
