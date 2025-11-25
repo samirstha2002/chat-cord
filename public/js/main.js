0959d57fd1b098e5b220e01c160f0f8fcea2b5a6
samirstha2002 <154325590+samirstha2002@users.noreply.github.com>
2025-11-25 14:31:17 +0545

    dom for form for chat

---
+++ public/js/main.js
@@ -0,0 +1,13 @@
+const chatForm = document.getElementById("chat-form");
+
+const socket = io();
+
+socket.on("message", (message) => {
+  console.log(message);
+});
+
+chatForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const msg = e.target.elements.msg.value;
+  console.log(msg);
+});
--- server.js
+++ server.js
@@ -1,22 +1,32 @@
 const express = require("express");
 const path = require("path");
 const dotenv = require("dotenv").config();
-const http=require('http')
-const socketio=require('socket.io')
-const app = express();
+const http = require("http");
+const socketio = require("socket.io");
 
-const server=http.createServer(app)
-const io= socketio(server)
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 // middle ware for static files
-
 app.use(express.static(path.join(__dirname, "public")));
 
 // run when clients connect
+io.on("connection", (socket) => {
+  //   console.log("NEW WS connection.... ");
 
-io.on("connection",socket=>{
-    console.log("NEW WS connection")
-})
+  // Welcome current user
+  socket.emit("message", "Welcome to ChatCord!");
+
+  // broadcast the all user except current user in room
+  socket.broadcast.emit("message", "user has joined the chat");
+
+  // disconnect the current user
+  socket.on("disconnect", () => {
+    // broadcast to all user including current one
+    io.emit("message", "user has left the chat");
+  });
+});
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
   console.log(` Server is  running on port ${port}`);
